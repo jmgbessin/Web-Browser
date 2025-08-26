0a9e0b8823ee13aaf01f2e1ad0918320cc10835a
Julien <jmgbessin@gmail.com>
2025-08-26 15:12:44 -0400

    started implementing DOM API with JS functions querySelectorAll and getAttribute through function exportation along with another effort to map python to JS: handles, numerical identifiers that match JS and python variables; node objects, for instance, when querySelectorAll is called

--- javascript.py
+++ javascript.py
@@ -1,14 +1,22 @@
 import dukpy
+from style import CSSParser
+from utils import *
 
 RUNTIME_JS = open("runtime.js").read()
 
 class JSContext:
-    def __init__(self):
+    def __init__(self, tab):
         self.interp = dukpy.JSInterpreter()
+        self.tab = tab
         
         self.interp.evaljs(RUNTIME_JS)
         
         self.interp.export_function("log", print)
+        self.interp.export_function("querySelectorAll", self.querySelectorAll)
+        self.interp.export_function("getAttribute", self.getAtribute)
+        
+        self.node_to_handle = {}
+        self.handle_to_node = {}
         
     def run(self, script, code):
         # try except block ensures browser doesn't crash due to Javascript crash
@@ -16,3 +24,27 @@ class JSContext:
             return self.interp.evaljs(code)
         except dukpy.JSRuntimeError as e:
             print("Script", script, "crashed", e)
+            
+    def querySelectorAll(self, selector_text):
+        selector = CSSParser(selector_text).selector()
+        nodes = [
+            node for node in tree_to_list(self.tab.nodes, [])
+            if selector.matches(node)
+            ]
+        return [self.get_handle(node) for node in nodes]
+    
+    def get_handle(self, elt):
+        if elt not in self.node_to_handle:
+            handle = len(self.node_to_handle)
+            self.node_to_handle[elt] = handle
+            self.handle_to_node[handle] = elt
+        else:
+            handle = self.node_to_handle[elt]
+        return handle
+    
+    def getAtribute(self, handle, attr):
+        elt = self.handle_to_node[handle]
+        attr = elt.attributes.get(attr, None)
+        return attr if attr else ""
+            
+        
--- runtime.js
+++ runtime.js
@@ -1,1 +1,11 @@
 console = { log: function(x) {call_python("log", x); } }
+document = { querySelectorAll: function(s) {
+    var handles = call_python("querySelectorAll", s);
+    return handles.map(function(h) {return new Node(h)});
+    }}
+
+function Node(handle) {this.handle = handle;}
+
+Node.prototype.getAttribute = function(attr) {
+    return call_python("getAttribute", this.handle, attr);
+}
